c59c1d55603a9e29523decf49f8bee1b063d79d1
Firaol-Terefa <firaolterefatolosa@gmail.com>
2025-07-11 15:52:13 +0300

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 // use whe
 // import rateLimiterMiddleware from './middleware/rateLimiter.js';
 
@@ -12,12 +13,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;  // default port is 5001
+const __dirname = path.resolve(); // get the current directory name
 
 //use middleware
-app.use(
-    cors({
-    orogin: "http://localhost:5173", // allow requests from this origin
-}));
+if (process.env.NODE_ENV !== "production") {
+        app.use(
+        cors({
+        origin: "http://localhost:5173", // allow requests from this origin
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);// apply rate limiting middleware
 // use th ebelow middleware when you need offline 
@@ -25,6 +30,14 @@ app.use(rateLimiter);// apply rate limiting middleware
 
 
 app.use("/api/notes", notesRoutes);
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")));  // ✅ Correct
+
+    app.get('*',(req,res) => {
+   res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  }
+);
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server started on PORT:", PORT);
